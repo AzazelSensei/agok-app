581736b906b5cd8778235aa19ff940ce04a721b8
AzazelSensei <89297042+AzazelSensei@users.noreply.github.com>
2022-06-10 02:52:18 +0300

    app(init): add theme

--- lib/core/extensions/widget_extension.dart
+++ lib/core/extensions/widget_extension.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 extension WidgetExtension on Widget {
   Widget addPadding() {
     return Padding(
-      padding: EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
       child: this,
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:agok_app/core/init/theme/colors_manager.dart';
+import 'package:agok_app/core/init/theme/theme_manager.dart';
+import 'package:agok_app/widget/mode_switcher.dart';
+import 'package:dynamic_themes/dynamic_themes.dart';
 import 'package:flutter/material.dart';
 import 'package:agok_app/core/extensions/int_extension.dart';
 import 'package:agok_app/core/extensions/widget_extension.dart';
@@ -12,36 +16,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return DynamicTheme(
+      builder: (BuildContext conetxt, ThemeData theme) => MaterialApp(
+        title: 'asdasd',
+        theme: theme,
+        debugShowCheckedModeBanner: false,
+        home: const MyHomePage(
+          title: 'Home',
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      themeCollection: ThemeCollection(themes: {
+        1: ThemeManager.theme,
+        0: ThemeManager.darkTheme,
+      }),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -51,6 +44,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Color? color, borderColor, fontColor;
 
   void _incrementCounter() {
     setState(() {
@@ -62,10 +56,22 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        actions: const [ModeSwitcher()],
+        title: Text(
+          widget.title,
+          style: TextStyle(
+              color: fontColor ??
+                  (ThemeManager.isDark(context)
+                      ? Colors.white
+                      : ColorManager.purple),
+              fontWeight: FontWeight.w700),
+        ),
       ),
       body: Container(
-        color: Colors.green,
+        color: color ??
+            (ThemeManager.isDark(context)
+                ? ColorManager.greyColor
+                : Colors.white),
       ).addPadding(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
